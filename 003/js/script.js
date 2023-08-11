4bb374352e1721e54137b4a4aa4b3d6340c9c2da
Jefferson <stupidjeff430@gmail.com>
2023-08-11 15:58:26 +0900

    add

---
+++ 003/js/script.js
@@ -0,0 +1,36 @@
+const { type } = require("express/lib/response");
+
+$(document).ready(function(){
+
+
+
+    // let catAPI = $.ajax({
+    //     url: "https://api.thecatapi.com/v1/images/" + ID + ".png",
+    //     type: "GET",
+    //     dataType: "json"
+    // });
+    // console.log(catAPI)
+
+    // let callback = (data) =>{
+    //     $(".image").html(catAPI.responseJSON)
+    // };
+    
+    const image = document.getElementById("img");
+    const catAPI ="https://api.thecatapi.com/v1/images/"
+
+    function generatePhoto(){
+        fetch(catAPI,{headers: {
+            'x-api-key': API_KEY
+          }})
+        .then(data =>{
+              return data.json();
+          })
+          .then(data => {
+              console.log(data.url);
+             image.src = data.url;
+              
+          })
+      }
+      
+      generatePhoto();
+});
