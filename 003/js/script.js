46b10c7900ae982ce550a491e818b077881b95fb
Jefferson <stupidjeff430@gmail.com>
2023-08-12 23:22:55 +0900

    add

--- 003/js/script.js
+++ 003/js/script.js
@@ -47,3 +47,18 @@
         
 //   });
 // })
+$(document).ready(function(){
+    const api_url ="https://zenquotes.io/api/quotes/";
+    
+    async function getapi(url)
+    {
+      const response = await fetch(url);
+      var data = await response.json();
+      console.log(data);
+    }
+    
+    getapi(api_url);
+
+    
+
+});
